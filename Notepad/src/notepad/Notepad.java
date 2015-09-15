a328c924054c5a2632745433bd510a630355f1a6
AmyGuliani <Aman.singh3796@gmail.com>
2015-09-15 11:23:17 +0530

    Notepad

---
+++ Notepad/src/notepad/Notepad.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepad;
+
+/**
+ *
+ * @author Amy
+ */
+public class Notepad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Frame().setVisible(true);
+        // TODO code application logic here
+    }
+}
