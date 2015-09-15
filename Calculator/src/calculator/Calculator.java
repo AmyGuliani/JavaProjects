228266579205f1b2ee66732547c220dff668d3ed
AmyGuliani <Aman.singh3796@gmail.com>
2015-09-15 11:07:15 +0530

    Calculator

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Amanpreet
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+        // TODO code application logic here
+    }
+}
